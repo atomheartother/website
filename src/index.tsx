97426a377d24b322f12d7496250037b227b66d1a
Élise N <atomheartother@gmail.com>
2022-04-06 19:46:16 -0400

    Update to use the new react 18.0 createroot

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
 import App from './App';
 
-ReactDOM.render(
-    <React.StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
-    </React.StrictMode>,
-    document.getElementById('root'),
-    );
+if (document === null) throw new Error("Document is null.");
+const rootDiv = document.getElementById("root");
+if (rootDiv === null) throw new Error("Root div couldn't be found.")
+const root = createRoot(rootDiv);
+
+root.render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+)
 // Hot Module Replacement (HMR) - Remove this snippet to remove HMR.
 // Learn more: https://snowpack.dev/concepts/hot-module-replacement
 if (import.meta.hot) {
